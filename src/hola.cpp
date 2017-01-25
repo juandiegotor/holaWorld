6b69497f4256a0b9f35c28b9375cb8cc9a7f9e22
Juan Diego <jdtorretron@gmail.com>
2017-01-25 13:01:14 -0500

    se cambio

--- src/hola.cpp
+++ src/hola.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <string>
 
-void hello(std::string s)
+std::string hello(std::string s)
 {
-	std::cout << "hello world " << s << std::endl;
+	return "hello world " + s;
 }
 
 
 int main(int argc, char **argv)
 {
+	std::string s;
 	for(int i = 1; i<argc; ++i)
 	{
-		hello(argv[i]);
+		s = hello(argv[i]);
+		std::cout << s << std::endl;
 	}
 }
